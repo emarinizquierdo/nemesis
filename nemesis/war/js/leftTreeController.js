cf91cea6bee68b1c82b2a0c4d422e9778f4edf83
emarinizquierdo <e.marin.izquierdo@gmail.com>
2012-07-18 18:04:44 +0200

    tree first aproximation

---
+++ nemesis/war/js/leftTreeController.js
@@ -0,0 +1,32 @@
+
+
+
+var leftTree = new function(){
+	
+	
+	function _init(){
+		
+		$("#treeview-left").kendoTreeView({
+            dragAndDrop: true,
+            dataSource: [
+                { text: "Furniture", expanded: true, items: [
+                    { text: "Tables & Chairs" },
+                    { text: "Sofas" },
+                    { text: "Occasional Furniture" }
+                ] },
+                { text: "Decor", items: [
+                    { text: "Bed Linen" },
+                    { text: "Curtains & Blinds" },
+                    { text: "Carpets" }
+                ] }
+            ]
+        }).data("kendoTreeView");
+	
+	
+	}
+	
+	this.init = _init;
+	
+}
+
+
