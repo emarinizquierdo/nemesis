f3092fbbc36625195ff9e789a4ac79704e4fbe89
emarinizquierdo <e.marin.izquierdo@gmail.com>
2012-07-19 20:46:08 +0200

    addElement to treeView

--- nemesis/war/js/controllers/leftTreeController.js
+++ nemesis/war/js/controllers/leftTreeController.js
@@ -3,29 +3,38 @@
 
 var leftTree = new function(){
 	
+	this.treeView;
 	
 	function _init(){
 		
-		$("#treeview-left").kendoTreeView({
+		this.treeView = $("#treeview-left").kendoTreeView({
             dragAndDrop: true,
             dataSource: [
-                { text: "Furniture", expanded: true, items: [
-                    { text: "Tables & Chairs" },
-                    { text: "Sofas" },
-                    { text: "Occasional Furniture" }
-                ] },
-                { text: "Decor", items: [
-                    { text: "Bed Linen" },
-                    { text: "Curtains & Blinds" },
-                    { text: "Carpets" }
-                ] }
+                
             ]
         }).data("kendoTreeView");
 	
 	
 	}
 	
+	
+	function _addElement(p_element){
+		
+		var lastElement = this.treeView.dataSource._data[this.treeView.dataSource.total()-1];
+		
+		if(typeof lastElement == "undefined"){
+			this.treeView.dataSource.add({text : p_element});
+		}else{
+			var node = this.treeView.findByUid(lastElement.uid);		
+			
+			this.treeView.insertAfter({ text: p_element }, node);		
+		}
+		
+		
+	}
+	
 	this.init = _init;
+	this.addElement = _addElement;
 	
 }
 
