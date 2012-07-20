fbe3e611638d00d4ddc2e98c47a92e7e6e27eeeb
emarinizquierdo <e.marin.izquierdo@gmail.com>
2012-07-20 02:13:23 +0200

    styles updated

--- nemesis/war/js/controllers/mainPanelController.js
+++ nemesis/war/js/controllers/mainPanelController.js
@@ -9,15 +9,15 @@ var mainPanel = new function(){
 		$("#splitterWrapper").kendoSplitter({
 		    panes: [
 		        { collapsible: false, min: "300px" },
-		       	{ collapsible: false, resizable: false, size: "1000px" },
+		       	{ collapsible: false, resizable: false, size: "700px" },
 		       	{ collapsible: false, min: "300px" }
 		    ],
 		    orientation: "horizontal"
 		});
 				$("#leftPanel").kendoSplitter({
 		    panes: [
-		        { size: "400px", collapsible: true, min: "300px" },
-		        { collapsible: true, min: "200px" }
+		        { collapsible: false, min: "300px" },
+		        { size: "285px",collapsible: true, max: "285px"}
 		    ],
 		    orientation: "vertical"
 		});
