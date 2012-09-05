9031d3354e985625689ffd2c089a4592ab3597e4
oscarsanguino <oscarsanguino@gmail.com>
2012-09-05 20:38:18 +0200

    scene controller

--- nemesis/war/js/controllers/sceneListController.js
+++ nemesis/war/js/controllers/sceneListController.js
@@ -1,41 +1,65 @@
 var sceneList = new function(){
 	
-	this.sceneView;
+	var _sceneView;
+	var _selected;
+	
+	var _SCENE_ITEM_TEMPLATE = "<img class='imgSnapshot' src='img/claqueta.jpg'>";
 	
 	function _init(){
 		
-
-		sceneList.sceneView = $("#sceneList").kendoListView({
-	          template: "<li>${FirstName} ${LastName}</li>",
-	          dataSource: {
-	              data: [
-	                  {
-	                      FirstName: "Joe",
-	                      LastName: "Smith"
-	                  },
-	                  {
-	                      FirstName: "Jane",
-	                      LastName: "Smith"
-	              }]
-	          }
-	      }).data("kendoListView");
+		//init list
+//		sceneList.sceneView = $("#sceneList").kendoListView({
+//
+//			  template: "<div class=scene-list-item><img class=imgSnapshot src=img/claqueta.jpg></div>"
+//	        , selectable: true
+//	        , change: _onChange
+//	      
+//		}).data("kendoListView");
 		
+		_sceneView = $('#sceneList');
+	
+		_add();
+		_selected = _sceneView.children().first();
+		_selected.addClass('selected');
+		
+		_sceneView.find('li.scene-list-item').live('click', function(){
+			
+			_selected.toggleClass('selected');
+			_selected = $(this);
+			_selected.toggleClass('selected');
+		})
 		
 		$("#snapshotButton").click(function(){
 			
 			var auxImgData = mainCanvas.canvas.toDataURL("png");
-			
-			$("#imgSnapshot").attr("src",auxImgData);
-			
+			_selected.find('img.imgSnapshot').attr("src", auxImgData);
 		});
 		
+		 $('#newSceneButton').click(function(){
 		
+			 _add();
+		 })
+		
+	}
+	
+	function _add(){
+		
+		 var newitem = $('<li>');
+		 newitem.addClass('scene-list-item');
+		 newitem.html(_SCENE_ITEM_TEMPLATE);
+		 _sceneView.append(newitem);
+	}
+	
+	function _onChange(e){
+		
+		this.select().addClass('selected')
 	}
 	
 	
 	
 	
 	this.init = _init;
+	this.add  = _add;
 	
 }
 
