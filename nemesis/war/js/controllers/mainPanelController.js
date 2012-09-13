bd0e2886b66f44cb9c7e61f139041ff41bf92af7
oscarsanguino <oscarsanguino@gmail.com>
2012-09-13 21:42:32 +0200

    resize app bug fixed

--- nemesis/war/js/controllers/mainPanelController.js
+++ nemesis/war/js/controllers/mainPanelController.js
@@ -3,15 +3,6 @@ var mainPanel = new function(){
 	
 	function _init(){
 		
-//		$("#splitterWrapper").kendoSplitter({
-//		    panes: [
-//		        { collapsible: false, min: "300px" },
-//		       	{ collapsible: false, resizable: false, size: "650px" },
-//		       	{ collapsible: false, min: "300px" }
-//		    ],
-//		    orientation: "horizontal"
-//		});
-		
 		$("#splitterWrapper").kendoSplitter({
 		    panes: [
 		        { size:"25%" },
@@ -22,32 +13,6 @@ var mainPanel = new function(){
 		});
 		
 		
-		
-//		$("#leftPanel").kendoSplitter({
-//		    panes: [
-//		        { size: "400px",collapsible: false},
-//		        { size: "685px",collapsible: false}
-//		    ],
-//		    orientation: "vertical"
-//		});
-		
-//		$("#leftPanel").kendoSplitter({
-//		    panes: [
-//		        { size: "50%", collapsible: false},
-//		        { size: "50%", collapsible: false}
-//		    ],
-//		    orientation: "vertical"
-//		});
-		
-		
-//		$("#middlePanel").kendoSplitter({
-//		    panes: [
-//		         {resizable: false, size: "550px", collapsible: false, min: "600px" },
-//		         {collapsible: false, min: "200px" }
-//		    ],
-//		    orientation: "vertical"
-//		});
-		
 		$("#middlePanel").kendoSplitter({
 		    panes: [
 		         {resizable: false, size: "450px" },
@@ -55,16 +20,6 @@ var mainPanel = new function(){
 		    ],
 		    orientation: "vertical"
 		});
-
-
-
-//		$("#rightPanel").kendoSplitter({
-//		    panes: [
-//		         { size: "450px", collapsible: true, min: "310px" },
-//		        { collapsible: true, min: "200px" }
-//		    ],
-//		    orientation: "vertical"
-//		});
 		
 		
 		$("#rightPanel").kendoSplitter({
@@ -83,5 +38,3 @@ var mainPanel = new function(){
 	this.init = _init;
 	
 }
-
-
